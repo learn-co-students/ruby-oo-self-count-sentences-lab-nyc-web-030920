d16ba45079b182e226a84eec22d3bf5d4825fbe0
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-03-10 16:45:32 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    prev = ""
+    sum = 0
+    self.each_byte do |b|
+      c = b.chr
+      if c == '.' or c == '?' or c == '!'
+        if prev.size > 0
+          sum += 1
+        end
+        prev = ""
+      else
+        prev += c
+      end
+    end
+    return sum
   end
+
 end
