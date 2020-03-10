7d0c5bcd1675d32a378316952cc0b0eff4d65ef1
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-10 21:22:58 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,32 @@
 require 'pry'
 
 class String
-
+  
+  def last_character
+    self.split("").last
+  end
+  
   def sentence?
-    
+    self.last_character == "."
   end
 
   def question?
-
+    self.last_character == "?"
   end
 
   def exclamation?
-
+    self.last_character == "!"
   end
 
   def count_sentences
-
+    counter = 0
+    words = self.split(" ")
+    
+    words.each do |word|
+      if word.include?(".") || word.include?("?") || word.include?("!")
+        counter += 1
+      end
+    end
+    counter
   end
 end
