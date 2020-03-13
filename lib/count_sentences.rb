b4d82a2306475b8c0b8e0b65453ddb706617c287
Vivica <Ma>
2020-03-13 01:29:47 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true 
+    else 
+      false 
+    end 
   end
 
   def question?
-
+    self.end_with?("?")? true: false 
+      
   end
 
   def exclamation?
-
+    self.end_with?("!")? true: false 
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|a| a.size <2}.size
   end
 end
